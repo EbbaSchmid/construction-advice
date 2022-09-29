8325a49d751cb380786f62469c4281992c39da1d
EbbaSchmid <justebba@gmail.com>
2022-09-29 18:16:44 -0500

    route set up

---
+++ routes/advices.js
@@ -0,0 +1,30 @@
+import { Router } from 'express'
+// import * as tacosCtrl from '../controllers/tacos.js'
+import { isLoggedIn } from '../middleware/middleware.js'
+
+const router = Router()
+
+// // GET localhost:3000/tacos
+// router.get('/', tacosCtrl.index)
+
+// // GET localhost:3000/tacos/:id
+// router.get('/:id', tacosCtrl.show)
+
+// // GET localhost:3000/tacos/:id/edit
+// router.get('/:id/edit', isLoggedIn, tacosCtrl.edit)
+
+// // POST localhost:3000/tacos
+// router.post('/', isLoggedIn, tacosCtrl.create)
+
+// // PATCH localhost:3000/tacos/:id/flip-tasty
+// router.patch('/:id/flip-tasty', isLoggedIn, tacosCtrl.flipTasty)
+
+// // PUT localhost:3000/tacos/:id
+// router.put('/:id', isLoggedIn, tacosCtrl.update)
+
+// // DELETE localhost:3000/tacos/:id
+// router.delete('/:id', isLoggedIn, tacosCtrl.delete)
+
+export {
+  router
+}
