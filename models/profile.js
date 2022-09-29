46cbe95a56db54f409b1f1f794da73849c6c30b6
EbbaSchmid <justebba@gmail.com>
2022-09-29 18:05:26 -0500

    view folder setup

---
+++ models/profile.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+// const catSchema = new Schema({
+//   name: String,
+//   age: Number,
+//   breed: String,
+// }, {
+//   timestamps: true
+// })
+
+const profileSchema = new Schema({
+  name: String,
+  avatar: String,
+  // cats: [catSchema]
+}, {
+  timestamps: true
+})
+
+const Profile = mongoose.model('Profile', profileSchema)
+
+export {
+  Profile
+}
